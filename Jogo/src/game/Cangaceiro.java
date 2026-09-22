74bef2ca94af8c04cdbd5f830bd59a7eef41ea96
randsonbredley <randsonbredley@gmail.com>
2026-09-21 23:51:23 -0300

    Adiciona simulação em Main
    Criar o ponto de entrada Main simulando as fases.
    
    Ajuste realizado: Utilizei o tipo genérico Location como tipo da variável de referência na simulação, reforçando o desacoplamento do cliente em relação às instâncias concretas.

--- Jogo/src/game/Cangaceiro.java
+++ Jogo/src/game/Cangaceiro.java
@@ -1,5 +1,8 @@
 package game;
 
-public class Cangaceiro {
-
+public class Cangaceiro implements Enemy {
+	@Override
+    public void attack() {
+        System.out.println("Cangaceiro do Sertão ataca com carabina e facão!");
+    }
 }
---
+++ Jogo/src/game/Main.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Main {
+	public static void main(String[] args) {
+        System.out.println("--- Iniciando Fase 1: Amazônia ---");
+        Location location = new AmazoniaLocation();
+        location.spawnEnemyAndAttack();
+
+        System.out.println("\n--- Iniciando Fase 2: Sertão ---");
+        location = new SertaoLocation();
+        location.spawnEnemyAndAttack();
+    }
+}
