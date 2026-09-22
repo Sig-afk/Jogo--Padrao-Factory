210fd4ca3dd2df21319a4f7228aef04f3a26e615
randsonbredley <randsonbredley@gmail.com>
2026-09-21 23:43:01 -0300

    Implementa MutantAnimal (inimigo da Amazônia)
    Criar a classe concreta que representa o animal mutante implementando Enemy.
    
    Ajuste realizado: Implementei MutantAnimal fornecendo uma mensagem personalizada no método attack() para simular o combate no console.

---
+++ Jogo/src/game/MutantAnimal.java
@@ -0,0 +1,8 @@
+package game;
+
+public class MutantAnimal implements Enemy {
+	@Override
+    public void attack() {
+        System.out.println("Animal Mutante da Amazônia ataca com garras e dentes ferozes!");
+    }
+}
